b506c1e5fd6e8c26b1f4facafff188b5afb7f788
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-10 22:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-//your JS code here. If required.
+ document.getElementById("btn").addEventListener("click", () => {
+      const one = Number(document.getElementById("ip").value);
+      const output = document.getElementById("output");
+
+      new Promise((resolve) => {
+        setTimeout(() => {
+          output.innerHTML = `Result: ${one}`;
+          resolve(one);
+        }, 2000);
+      })
+      .then((val) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const result = val * 2;
+            output.innerHTML = `Result: ${result}`;
+            resolve(result);
+          }, 2000);
+        });
+      })
+      .then((val) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const result = val - 3;
+            output.innerHTML = `Result: ${result}`;
+            resolve(result);
+          }, 3000);
+        });
+      })
+      .then((val) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const result = val - 2;
+            output.innerHTML = `Result: ${result}`;
+            resolve(result);
+          }, 1000);
+        });
+      })
+      .then((val) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const result = val / 2;
+            output.innerHTML = `Result: ${result}`;
+            resolve(result);
+          }, 1000);
+        });
+      })
+      .then((val) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const result = val + 10;
+            output.innerHTML = `Final Result: ${result}`;
+            resolve(result);
+          }, 1000);
+        });
+      });
+    });
